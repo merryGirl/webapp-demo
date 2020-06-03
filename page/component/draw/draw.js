4c6170b7620d037dda895b71838b2e4789f079b1
mingui.qian <2283906227@qq.com>
2020-06-03 18:31:45 +0800

    点击贴图放置到绘图区init

--- page/component/draw/draw.js
+++ page/component/draw/draw.js
@@ -13,23 +13,48 @@ Page({
       //     id: 1,
       //     name: '绘画',
       //     src: '../../../image/21.png',
-      //     zIndex: 0
       //   },{
       //     id: 2,
       //     name: '中心',
       //     src: '../../../image/31.png',
-      //     zIndex: 0
       //   }]
       // }
-    ],
+    ], // 贴图数据
     sourceTabs: [], // 请求tab
-    activeSource: 'logo'
+    activeSource: 'logo', // 当前激活tab
+    imgLocations: [
+      // { 
+      //   id: 0,
+      //   left: 0,
+      //   top: 0,
+      //   width: 70,
+      //   height: 70,
+      //   rotate: 0, // 旋转角度
+      //   url: '', //img链接
+      //   zIndex: 0
+      // } // 数据格式
+    ], // 图片定位
+    recordLocations: [
+      // {
+      //   w: 70,  //img宽度
+      //   h: 70,   //img高度
+      //   lastLeft: 0, //imgx轴的值
+      //   lastTop: 0, //imgy轴的值
+      //   rotate: 0,  //img旋转角度
+      //   sx: 0, // 源x轴方向
+      //   sy: 0, // 源y轴
+      //   scx: 0,
+      //   r: 0,
+      //   rox: 0,
+      //   per: 1,  //img宽高比
+      // }
+    ], // 图片移动位置记录
+    activeImgId: null, // 当前激活图片id
   },
   onLoad() {
     this.getImgsReq()
   },
   selectImgType(e) {
-    console.log('切换active',  e.target.dataset.name)
     this.setData({
       activeSource: e.target.dataset.name
     })
@@ -51,5 +76,51 @@ Page({
       }).catch(e => {
         console.log(e)
       })
-  }
+  },
+
+  selecedImg(e) {
+    console.log('当前点击的切图', e.target.dataset)
+    const dataset = e.target.dataset
+    const loc = {
+      id: dataset.img.id,
+      left: 0,
+      top: 0,
+      width: dataset.item.width*2,
+      height: dataset.item.height*2,
+      rotate: 0, // 旋转角度
+      url: dataset.img.src, //img链接
+      zIndex: 0
+    }
+    this.setData({
+      imgLocations: this.data.imgLocations.concat(loc)
+    })
+  },
+
+  imgStart(e) {
+
+  },
+
+  imgMove(e) {
+
+  },
+
+  rotateStart(e) {
+
+  },
+
+  rotateMove(e) {
+
+  },
+
+  rotateEnd(e) {
+
+  },
+
+  scaleStart(e) {
+
+  },
+
+  scaleMove(e) {
+
+  },
 })
